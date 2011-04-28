814661f549618554b881f9251f9548aecabbc3f6
sdomino <domino.steve@gmail.com>
2011-04-28 09:53:40 -0600

    master - NEW: updating file structure

--- lib/compressit.rb
+++ lib/compressit.rb
@@ -1,46 +1,50 @@
+require 'rails'
+
 module Compressit
+  class Railtie < Rails::Railtie
   
-  require 'compressit/railtie' if defined?(Rails)
+    require 'compressit/railtie' if defined?(Rails)
   
-  def self.css
-    @files_to_compress   = Dir.glob("#{Dir.new("#{Rails.root}/public/stylesheets")}/**/*.css")
-    @destination_path    = Dir.open("#{Rails.root}/public/stylesheets/compressed")
-    # version update somehow
-    @version             = CSS_VERSION
-    @ext                 = ".css"
-    @compressed          = "compressed-#{@version}#{@ext}"
+    def self.css
+      @files_to_compress   = Dir.glob("#{Dir.new("#{Rails.root}/public/stylesheets")}/**/*.css")
+      @destination_path    = Dir.open("#{Rails.root}/public/stylesheets/compressed")
+      # version update somehow
+      @version             = CSS_VERSION
+      @ext                 = ".css"
+      @compressed          = "compressed-#{@version}#{@ext}"
 
-    compressit
-  end
-
-  def self.js
-    @files_to_compress   = Dir.glob("#{Dir.new("#{Rails.root}/public/javascripts")}/**/*.js")
-    @destination_path    = Dir.open("#{Rails.root}/public/javascripts/compressed")
-    # version update somehow
-    @version             = JS_VERSION
-    @ext                 = ".js"
-    @compressed          = "compressed-#{@version}#{@ext}"
-
-    compressit
-  end
-
-  private
-
-  def compressit
-    @java                = "/usr/bin/java"
-    @yuicompressor       = "#{File.dirname(__FILE__)}/yuicompressor-2.4.2.jar"
-
-    # remove current file if for some reason it already exists
-    `rm -f #{File.path(@destination_path)}/#{@compressed}` if File.exists?("#{File.path(@destination_path)}/#{@compressed}")
-
-    # compress each file in base_path and save the compressed file to destination_path
-    @files_to_compress.each do |file|
-      `#{@java} -jar #{@yuicompressor} #{file} >> #{File.path(@destination_path)}/#{@compressed}`
-      puts "Added: #{File.basename(file)}... \n"
+      compressit
     end
 
-    # confirm compression and show destination path where file can be found
-    puts "Complete! Compressed #{@ext} file: '#{@compressed}' can be found in '#{File.path(@destination_path)}'"
-  end
+    def self.js
+      @files_to_compress   = Dir.glob("#{Dir.new("#{Rails.root}/public/javascripts")}/**/*.js")
+      @destination_path    = Dir.open("#{Rails.root}/public/javascripts/compressed")
+      # version update somehow
+      @version             = JS_VERSION
+      @ext                 = ".js"
+      @compressed          = "compressed-#{@version}#{@ext}"
+
+      compressit
+    end
+
+    private
+
+    def compressit
+      @java                = "/usr/bin/java"
+      @yuicompressor       = "#{File.dirname(__FILE__)}/yuicompressor-2.4.2.jar"
+
+      # remove current file if for some reason it already exists
+      `rm -f #{File.path(@destination_path)}/#{@compressed}` if File.exists?("#{File.path(@destination_path)}/#{@compressed}")
+
+      # compress each file in base_path and save the compressed file to destination_path
+      @files_to_compress.each do |file|
+        `#{@java} -jar #{@yuicompressor} #{file} >> #{File.path(@destination_path)}/#{@compressed}`
+        puts "Added: #{File.basename(file)}... \n"
+      end
+
+      # confirm compression and show destination path where file can be found
+      puts "Complete! Compressed #{@ext} file: '#{@compressed}' can be found in '#{File.path(@destination_path)}'"
+    end
   
+  end
 end
---
+++ lib/tasks/compressit.rake
@@ -0,0 +1,29 @@
+namespace :compressit do
+  desc "Create global version variables"
+  task :install do
+    # File.open("#{Rails.root}/config/initializers/compressit.rb", "w+") do |file|
+    #     file.puts "CSS_VERSION = '1.0.0'"
+    #     file.puts "JS_VERSION = '1.0.0'"
+    #   end
+    puts "do stuff"
+  end
+  
+  namespace :version do
+    desc "Version css file before compression"
+    task :css => :install do
+      puts "version bump major, minor, patch"
+      puts "update version of css or js"
+    end
+    
+    desc "Version js file before compression"
+    task :js => :install do
+      puts "version bump major, minor, patch"
+      puts "update version of css or js"
+    end
+  end
+
+  desc "Compress file"
+  task :compress => :version do
+    puts "compress css or js"
+  end
+end
