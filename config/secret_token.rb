fbebb7a538b02fa02f197a3d4bb76baf74ccdfb0
sdomino <domino.steve@gmail.com>
2011-05-02 10:48:46 -0600

    master - NEW: removing config file

--- config/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gemtest::Application.config.secret_token = 'f9dfbd1c0cd338a4553ce552fc47fd5641232813966009b8185e2284782e31176fa54ba329d1ee20fed119fa6e66a2688e409bd3a16c0bf1116be709869be617'
