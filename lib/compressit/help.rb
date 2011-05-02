2cd27a0268983fa06e6013bbde31f5ca6fe2a149
sdomino <domino.steve@gmail.com>
2011-05-01 21:14:30 -0600

    master - NEW: adding help commands

---
+++ lib/compressit/help.rb
@@ -0,0 +1,10 @@
+module Compressit
+  def index
+    display %{
+      === Usage ===
+
+      help                         # show this usage
+      version                      # show the gem version
+    }
+  end
+end
