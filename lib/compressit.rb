f01d2dc0932287951649e7ed208fdb4bc9519e12
sdomino <domino.steve@gmail.com>
2011-08-01 23:32:03 -0600

    master - NEW: refactoring

--- lib/compressit.rb
+++ lib/compressit.rb
@@ -12,28 +12,27 @@ module Compressit
         
         opts.banner = "Usage: compressit [option] [FOLDER]"
 
-        opts.on('-h', '--help', 'Display help') do
-          Compressit::Base.show_usage
+        opts.on('-h', '--help', 'Display this help') do
+          puts optparser
         end
         opts.on('-v', '--version', 'Display current gem version') do
           puts "Currently using version: #{VERSION}"
         end
-        opts.on( '-c', '--css FOLDER', 'Compress css files from specified folder' ) do
-          puts "Compressing css files from '#{arguments[0]}' into '#{arguments[0]}/compressed'"
-          options[:folder] = arguments[0]
-          Compressit::Base.css(options)
+        opts.on( '-c', '--css FOLDER VERSION', 'Compress css files from specified folder' ) do
+          puts "Compressing css files from '#{arguments[0]}' into '#{arguments[0]}/compressed/compressed-#{arguments[1]}.css'"
+          options[:folder], options[:version], options[:ext] = arguments[0], arguments[1], '.css'
+          Compressit::Base.compressit(options)
         end
-        opts.on( '-j', '--js FOLDER', 'Compress javascript files from specified folder' ) do
-          puts "Compressing javascript files from '#{arguments[0]}' into '#{arguments[0]}/compressed'"
-          options[:folder] = arguments[0]
-          Compressit::Base.js(options)
+        opts.on( '-j', '--js FOLDER VERSION', 'Compress javascript files from specified folder' ) do
+          puts "Compressing javascript files from '#{arguments[0]}' into '#{arguments[0]}/compressed/compressed-#{arguments[1]}.js'"
+          options[:folder], options[:version], options[:ext] = arguments[0], arguments[1], '.js'
+          Compressit::Base.compressit(options)
         end
       end
       
       begin optparser.parse!(arguments)
       rescue OptionParser::InvalidOption => error
         puts "Oops! #{error}, try this: "
-        # Compressit::Base.show_usage
         puts optparser
         exit 1
       end
--- lib/compressit/base.rb
+++ lib/compressit/base.rb
@@ -2,51 +2,13 @@ module Compressit
   module Base
     class << self
 
-      def setup(options = nil)
-        @java = '/usr/bin/java'
-        @yuicompressor = File.expand_path(File.dirname(__FILE__) + './../yuicompressor-2.4.6.jar')
+      def compressit(options)
         
-        @version = "#{options[:folder]}/compressed/VERSION.txt"
+        @version, @ext, @folder  = options[:version], options[:ext], options[:folder]
         
-        puts "VERSION: #{@version}"
-        unless File.exists?(@version)
-          File.open(@version, 'w') do |file|
-            file.puts '1.0.0'
-          end
-        end
-      end
-      
-      def css(options = nil)
-        
-        setup
-        
-        @files_to_compress          = Dir.glob("#{options[:folder]}/**/*.css")
-        @destination_path           = Dir.exists?("#{options[:folder]}/compressed") ? Dir.open("#{options[:folder]}/compressed") : Dir.mkdir("#{options[:folder]}/compressed")
-        
-        @css_version  = file.open(@version, 'rb') {|file| file.read}
-        
-        @ext                        = '.css'
-        @compressed                 = "compressed-#{@css_version}#{@ext}"
-        
-        compressit
-      end
-
-      def js(options = nil)
-        
-        setup
-        
-        @files_to_compress          = Dir.glob("#{options[:folder]}/**/*.js")
-        @destination_path           = Dir.new("#{options[:folder]}/compressed") || Dir.mkdir("#{options[:folder]}/compressed")
-        
-        @js_version = file.open(@version, 'rb') {|file| file.read}
-        
-        @ext                        = '.js'
-        @compressed                 = "compressed-#{@js_version}.#{@ext}"
-        
-        compressit
-      end
-
-      def compressit
+        @java, @yuicompressor             = '/usr/bin/java', File.expand_path(File.dirname(__FILE__) + './../yuicompressor-2.4.6.jar')
+        @destination_path                 = Dir.exists?("#{@folder}/compressed") ? Dir.open("#{@folder}/compressed") : Dir.mkdir("#{@folder}/compressed")
+        @files_to_compress, @compressed   = Dir.glob("#{@folder}/**/*#{@ext}"), "compressed-#{@version}#{@ext}"
         
         # remove compressed file if the version hasn't been updated to avoid duplicate compression
         `rm -f #{File.path(@destination_path)}/#{@compressed}` if File.exists?("#{File.path(@destination_path)}/#{@compressed}")
@@ -71,27 +33,8 @@ module Compressit
         #   # confirm compression and show destination path where file can be found
         #   puts "Complete! Compressed #{@ext} file '#{@compressed}', can be found in '#{File.path(@destination_path)}'"
         # rescue
-        #   puts "Hmm... looks like this foulder doesn't contain any #{@ext} files."
+        #   puts "Hmm... looks like this folder doesn't contain any #{@ext} files."
         # end
-      end
-      
-      # add the ability to see css/js version. do 
-      
-      def show_usage
-        puts %{
-          --- Usage ---
-          
-          --- CLI ---
-          -h, --help                      # show this usage
-          -s, --setup                     # create initial .css and .js version files
-          -v, --version                   # show the current gem version
-                                         
-          -c, --css                       # compress .css files with the version specified in the config file
-          -cv, --cssv                     # display current compressed css file version
-          
-          -j, --js                        # compress .js files with the version specified in the config file
-          -jv, --jsv                      # display durrent compressed javascript file version
-        }
       end
       
     end
