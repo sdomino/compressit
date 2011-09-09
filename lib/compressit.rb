e2a33c58d9c2d2e7e4321e90aa4f3c270b9ec767
Domino <domino.steve@gmail.com>
2011-09-09 09:11:34 -0600

    master - FIX: quick fix to new cli

--- lib/compressit.rb
+++ lib/compressit.rb
@@ -6,43 +6,39 @@ module Compressit
   class << self
   
     def run(arguments)
-      options = Hash.new
-      
-      optparser = OptionParser.new do|opts|
+      unless arguments.empty? || !arguments[0].include?('-')
+        options = Hash.new
         
-        opts.banner = "Usage: compressit [option] [FOLDER] [VERSION]"
+        optparser = OptionParser.new do|opts|
+          
+          opts.banner = "Usage: compressit [option] [FOLDER] [VERSION]"
 
-        arguments.each do |argument|
-          if argument.empty? || !argument.include?('-')
-            opts.on('') do
-              puts optparser
-            end
+          opts.on('-h', '--help', 'Display this help') do
+            puts optparser
+          end
+          opts.on('-v', '--version', 'Display current gem version') do
+            puts "Currently using version: #{VERSION}"
+          end
+          opts.on('-c', '--css FOLDER VERSION', 'Compress css files from [FOLDER] into [FOLDER]/compressed') do
+            puts "Compressing css files from '#{arguments[0]}' into '#{arguments[0]}/compressed/compressed-#{arguments[1]}.css'"
+            options[:folder], options[:version], options[:ext] = arguments[0], arguments[1], '.css'
+            Compressit::Base.compressit(options)
+          end
+          opts.on('-j', '--js FOLDER VERSION', 'Compress javascript files from [FOLDER] into [FOLDER]/compressed') do
+            puts "Compressing javascript files from '#{arguments[0]}' into '#{arguments[0]}/compressed/compressed-#{arguments[1]}.js'"
+            options[:folder], options[:version], options[:ext] = arguments[0], arguments[1], '.js'
+            Compressit::Base.compressit(options)
           end
         end
-
-        opts.on('-h', '--help', 'Display this help') do
+        
+        begin optparser.parse!(arguments)
+        rescue OptionParser::InvalidOption => error
+          puts "#{error}, try one of this: "
           puts optparser
+          exit 1
         end
-        opts.on('-v', '--version', 'Display current gem version') do
-          puts "Currently using version: #{VERSION}"
-        end
-        opts.on('-c', '--css FOLDER VERSION', 'Compress css files from [FOLDER] into [FOLDER]/compressed') do
-          puts "Compressing css files from '#{arguments[0]}' into '#{arguments[0]}/compressed/compressed-#{arguments[1]}.css'"
-          options[:folder], options[:version], options[:ext] = arguments[0], arguments[1], '.css'
-          Compressit::Base.compressit(options)
-        end
-        opts.on('-j', '--js FOLDER VERSION', 'Compress javascript files from [FOLDER] into [FOLDER]/compressed') do
-          puts "Compressing javascript files from '#{arguments[0]}' into '#{arguments[0]}/compressed/compressed-#{arguments[1]}.js'"
-          options[:folder], options[:version], options[:ext] = arguments[0], arguments[1], '.js'
-          Compressit::Base.compressit(options)
-        end
-      end
-      
-      begin optparser.parse!(arguments)
-      rescue OptionParser::InvalidOption => error
-        puts "#{error}, try one of this: "
-        puts optparser
-        exit 1
+      else
+        puts `compressit -h`
       end
     end
   
